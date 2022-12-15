cfa8ed8f825feb2d5c0130f02cb1e67a5f8162c0
Guilherme Oliveira <guilherme.moliveira@hotmail.com>
2022-12-15 15:09:46 -0300

    feat: Requisito 1 - Implementa Teste para verificar o inicializador do objeto Produto

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,21 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui//
+    product = Product(
+        1,
+        "Iogurte de Morango",
+        "Itambé",
+        "2022-12-12",
+        "2023-05-22",
+        "5678",
+        "Refrigerado",
+    )
+
+    assert product.id == 1
+    assert product.nome_do_produto == "Iogurte de Morango"
+    assert product.nome_da_empresa == "Itambé"
+    assert product.data_de_fabricacao == "2022-12-12"
+    assert product.data_de_validade == "2023-05-22"
+    assert product.numero_de_serie == "5678"
+    assert product.instrucoes_de_armazenamento == "Refrigerado"

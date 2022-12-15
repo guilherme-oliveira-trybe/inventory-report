8bb32b33af713ee402902fad4437189a070d2b9e
Guilherme Oliveira <guilherme.moliveira@hotmail.com>
2022-12-15 20:01:08 -0300

    feat: Requisito 2 - Implementa método generate na classe  SimpleReport retornando um relatório simples

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,2 +1,47 @@
+from datetime import date
+from collections import Counter
+
+
 class SimpleReport:
-    pass
+    @staticmethod
+    def generate(report):
+        today = date.today()
+        manufacturing_dates = [item["data_de_fabricacao"] for item in report]
+        expiration_date = [
+            item["data_de_validade"]
+            for item in report
+            if item["data_de_validade"] > str(today)
+        ]
+        all_companies = Counter([item["nome_da_empresa"] for item in report])
+
+        return (
+            f"Data de fabricação mais antiga: {min(manufacturing_dates)}\n"
+            f"Data de validade mais próxima: {min(expiration_date)}\n"
+            f"Empresa com mais produtos: {all_companies.most_common()[0][0]}"
+        )
+
+
+print(
+    SimpleReport.generate(
+        [
+            {
+                "id": 1,
+                "nome_do_produto": "CADEIRA",
+                "nome_da_empresa": "Forces of Nature",
+                "data_de_fabricacao": "2022-04-04",
+                "data_de_validade": "2023-02-09",
+                "numero_de_serie": "FR48",
+                "instrucoes_de_armazenamento": "Conservar em local fresco",
+            },
+            {
+                "id": 2,
+                "nome_do_produto": "CADEIRA",
+                "nome_da_empresa": "Forces of Nature",
+                "data_de_fabricacao": "2022-02-03",
+                "data_de_validade": "2023-09-10",
+                "numero_de_serie": "FR48",
+                "instrucoes_de_armazenamento": "Conservar em local fresco",
+            },
+        ]
+    )
+)

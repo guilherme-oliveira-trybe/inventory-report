56dad1d0f806785782eb7118bd03ce9393bef5e7
Guilherme Oliveira <guilherme.moliveira@hotmail.com>
2023-01-09 12:09:47 -0300

    refactor(simple_report): Altera de método estático para método de classe e retira código de teste manual

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -3,8 +3,8 @@ from collections import Counter
 
 
 class SimpleReport:
-    @staticmethod
-    def generate(report):
+    @classmethod
+    def generate(cls, report):
         today = date.today()
         manufacturing_dates = [item["data_de_fabricacao"] for item in report]
         expiration_date = [
@@ -19,29 +19,3 @@ class SimpleReport:
             f"Data de validade mais próxima: {min(expiration_date)}\n"
             f"Empresa com mais produtos: {all_companies.most_common()[0][0]}"
         )
-
-
-print(
-    SimpleReport.generate(
-        [
-            {
-                "id": 1,
-                "nome_do_produto": "CADEIRA",
-                "nome_da_empresa": "Forces of Nature",
-                "data_de_fabricacao": "2022-04-04",
-                "data_de_validade": "2023-02-09",
-                "numero_de_serie": "FR48",
-                "instrucoes_de_armazenamento": "Conservar em local fresco",
-            },
-            {
-                "id": 2,
-                "nome_do_produto": "CADEIRA",
-                "nome_da_empresa": "Forces of Nature",
-                "data_de_fabricacao": "2022-02-03",
-                "data_de_validade": "2023-09-10",
-                "numero_de_serie": "FR48",
-                "instrucoes_de_armazenamento": "Conservar em local fresco",
-            },
-        ]
-    )
-)

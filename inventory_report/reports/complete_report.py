6ea8546cd9ba41700120afa04665561b68541c0f
Guilherme Oliveira <guilherme.moliveira@hotmail.com>
2023-01-09 12:12:07 -0300

    feat: Versão completa do relatório, através da classe CompleteReport herdando da classe SimpleReport

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,2 +1,14 @@
-class CompleteReport:
-    pass
+from inventory_report.reports.simple_report import SimpleReport
+from collections import Counter
+
+
+class CompleteReport(SimpleReport):
+    @classmethod
+    def generate(cls, report):
+        simple_report = super().generate(report)
+        all_companies = Counter([item["nome_da_empresa"] for item in report])
+        complete_report = simple_report + "\nProdutos estocados por empresa:\n"
+        for company, quantity in all_companies.items():
+            complete_report += f"- {company}: {quantity}\n"
+
+        return complete_report
